fa273bd302215f2e765cd104fae49b4a0908c997
Leticia Domingues <leticia.domingues@hotmail.com>
2017-08-11 23:01:52 -0300

    Pg inicial - listagem de alunos

---
+++ sistemaAluno/app/Http/Controllers/AlunoController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Aluno;
+
+class AlunoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $alunos = Aluno::all();
+        //\DB::table('aluno')->get();
+        return view('aluno.index')->with(['alunos'=>$alunos]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $aluno = Aluno::find($id);
+        return view('aluno.edit')->with(['aluno'=>$aluno]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- sistemaAluno/database/migrations/2017_08_11_200504_createTableAluno.php
+++ sistemaAluno/database/migrations/2017_08_11_200504_createTableAluno.php
@@ -13,7 +13,7 @@ class CreateTableAluno extends Migration
      */
     public function up()
     {
-        Schema::create('aluno', function (Blueprint $table) {
+        Schema::create('alunos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome',100);
             $table->date('dataNasc');
